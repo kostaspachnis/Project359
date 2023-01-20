c8f97cdf4ecdbc39c637b55a870685981154253b
PetrosTsal <petros.tsalikis98@gmail.com>
2023-01-20 20:05:10 +0200

    frontend signup

--- web/js/signup_librarian.js
+++ web/js/signup_librarian.js
@@ -2,3 +2,26 @@
 function cancel() {
     window.location = "librarian.html";
 }
+
+
+function RegisterNewLibrarian() {
+        
+    let myForm = document.getElementById('myForm');
+    let formData = new FormData(myForm);
+    const data = {};
+    formData.forEach((value, key) => (data[key] = value));
+    var jsonData = JSON.stringify(data);
+    var xhr = new XMLHttpRequest();
+    console.log(jsonData);
+    xhr.onload = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            $('#ajaxContent').html("Successful Librarian Registration. Welcome!");
+            setTimeout(function(){window.location = "librarian.html";}, 3000);
+        } else if (xhr.status !== 200) {
+            $('#ajaxContent').html("ERROR!");
+        }
+    };
+    xhr.open('POST', 'RegisterLibrarian');
+    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    xhr.send(jsonData);
+}
--- web/js/signup_student.js
+++ web/js/signup_student.js
@@ -2,3 +2,25 @@
 function cancel() {
     window.location = "student.html";
 }
+
+
+function RegisterNewStudent() {
+    let myForm = document.getElementById('myForm');
+    let formData = new FormData(myForm);
+    const data = {};
+    formData.forEach((value, key) => (data[key] = value));
+    var jsonData = JSON.stringify(data);
+    var xhr = new XMLHttpRequest();
+    console.log(jsonData);
+    xhr.onload = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            $('#ajaxContent').html("Successful Student Registration. Welcome! ");
+            setTimeout(function(){window.location = "student.html";}, 3000);
+        } else if (xhr.status !== 200) {
+            $('#ajaxContent').html("ERROR!");
+        }
+    };
+    xhr.open('POST', 'RegisterStudent');
+    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    xhr.send(jsonData);
+}
