7664b14458c80c62428d1327e4d706357e1491e0
PetrosTsal <petros.tsalikis98@gmail.com>
2023-01-21 16:29:37 +0200

    frontend search books

--- web/js/student.js
+++ web/js/student.js
@@ -8,11 +8,21 @@ function signup() {
 
 function searchBook() {
 
-    let myForm = document.getElementById('bookSearchForm');
-    let formData = new FormData(myForm);
-    const data = {};
-    formData.forEach((value, key) => (data[key] = value));
-    var jsonData = JSON.stringify(data);
+    // let myForm = document.getElementById('bookSearchForm');
+    // let formData = new FormData(myForm);
+    // const data = {};
+    // formData.forEach((value, key) => (data[key] = value));
+    // var jsonData = JSON.stringify(data);
+
+    let genre = document.getElementById('search_genre').value;
+    let title = document.getElementById('search_title').value;
+    let author = document.getElementById('search_author').value;
+    let fromPage = document.getElementById('fromPageNumber').value;
+    let toPage = document.getElementById('toPageNumber').value;
+    let fromYear = document.getElementById('fromYear').value;
+    let toYear = document.getElementById('toYear').value;
+
+    let data = 'genre=' + genre + '&title=' + title + '&author=' + author + '&fromPage=' + fromPage + '&toPage=' + toPage + '&fromYear=' + fromYear + '&toYear=' + toYear;
 
     var xhr = new XMLHttpRequest();
 
@@ -25,9 +35,8 @@ function searchBook() {
         }
     };
 
-    // var data = $('#bookSearchForm').serialize();
 
-    xhr.open('GET', 'FindBookForStudent?');
+    xhr.open('GET', 'FindBookForStudent?'+data);
     xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-    xhr.send(jsonData);
+    xhr.send();
 }
