8ddd5fbd521aa9e141f13ee6e142e2b63bb882fe
PetrosTsal <petros.tsalikis98@gmail.com>
2023-01-21 15:47:46 +0200

    frontend book search

--- web/js/student.js
+++ web/js/student.js
@@ -5,3 +5,28 @@ function signup() {
     window.location = "signup_student.html";
 }
 
+
+function searchBook() {
+
+    let myForm = document.getElementById('bookSearchForm');
+    let formData = new FormData(myForm);
+    const data = {};
+    formData.forEach((value, key) => (data[key] = value));
+    var jsonData = JSON.stringify(data);
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.onload = function () {
+        if (xhr.readyState === 4 && xhr.status === 202) {
+            console.log("success");
+        } else if (xhr.status !== 202) {
+            console.log("error");
+        }
+    };
+
+    // var data = $('#bookSearchForm').serialize();
+
+    xhr.open('GET', 'FindBookForStudent?');
+    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    xhr.send(jsonData);
+}
