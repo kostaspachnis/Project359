1569acf233fa2d70091b0c7c6ffbe7b1c4d9d928
PetrosTsal <petros.tsalikis98@gmail.com>
2023-01-22 19:48:05 +0200

    frontend show libs

--- web/js/student.js
+++ web/js/student.js
@@ -177,11 +177,9 @@ function createLibraryList(jsonData) {
     function () {
         if (this.readyState === this.DONE) {
             results=JSON.parse(this.responseText);
-            distances = results.distances;
-            durations = results.durations;
             for(var i = 0; i < bookList.length; i++) {
-                bookList[i].distance = distances[i];
-                bookList[i].duration = durations[i];
+                bookList[i].distance = results.distances[1][i];
+                bookList[i].duration = results.durations[1][i];
             }
             createClosestLibrariesList(bookList);
         }
