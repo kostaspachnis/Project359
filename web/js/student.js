a9d093c439f9d8b951ad1cf50f878aeeef489a2f
PetrosTsal <petros.tsalikis98@gmail.com>
2023-01-22 18:04:29 +0200

    frontend show libs

--- web/js/student.js
+++ web/js/student.js
@@ -167,10 +167,10 @@ function createLibraryList(jsonData) {
     let destinations = '&destinations=';
 
     origins += mylat + '%2C-' + mylon;
-    destinations += bookList[0].lat + '%2C-' + bookList[0].lon + '%';
+    destinations += bookList[0].lat + '%2C' + bookList[0].lon + '%';
 
     for(var i = 1; i < bookList.length; i++) {
-        destinations += i===bookList.length-1 ? '3B' + bookList[i].lat + '%2C-' + bookList[i].lon : '3B' + bookList[i].lat + '%2C-' + bookList[i].lon + '%';
+        destinations += i===bookList.length-1 ? '3B' + bookList[i].lat + '%2C' + bookList[i].lon : '3B' + bookList[i].lat + '%2C' + bookList[i].lon + '%';
     }
 
     console.log(origins);
