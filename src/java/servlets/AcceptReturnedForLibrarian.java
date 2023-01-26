d388cc6ead3f6f9373ed3764990fc8442d57f428
Kostas Pachnis <csd4100@csd.uoc.gr>
2023-01-26 19:16:26 +0200

    backend: librarian accept requested v2

--- src/java/servlets/AcceptReturnedForLibrarian.java
+++ src/java/servlets/AcceptReturnedForLibrarian.java
@@ -75,6 +75,7 @@ public class AcceptReturnedForLibrarian extends HttpServlet {
             if (bil.getAvailable().equals("false")) {
                 int bcid = bil.getBookcopy_id();
                 bortable.updateBorrowingLib(bcid, "successEnd");
+                bil.setAvailable("true");
                 response.setStatus(200);
             } else {
                 response.setStatus(403);
