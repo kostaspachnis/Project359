cfe2081f727c85d01951b975cffb8769c2483a67
PetrosTsal <petros.tsalikis98@gmail.com>
2023-01-26 23:37:03 +0200

    frontend update

--- web/js/librarian.js
+++ web/js/librarian.js
@@ -388,7 +388,7 @@ function updateLibrarian() {
 
     var data = $('#myForm').serialize();
 
-    xhr.open('POST', 'UpdateLibrarian' + data);
+    xhr.open('POST', 'UpdateLibrarian?' + data);
     xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
     xhr.send(data);
 }
